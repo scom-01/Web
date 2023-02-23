9beff985189d09e546e90b5213bb8ac3fe4f2cef
KDH <ekgns0131@daum.net>
2023-02-24 04:58:59 +0900

    Add App.module.css

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -1,9 +1,12 @@
 import Button from "./Button";
+import style from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1 className={style.title} id={style.title}>
+        Welcome back!
+      </h1>
       <Button text={"Continue"} />
     </div>
   );

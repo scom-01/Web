a0006faab97cd20671f5457f3354fbeb518b865c
KDH <ekgns0131@daum.net>
2023-02-24 16:33:09 +0900

    useEffect 원하는 함수를 한 번만 실행 시키고자 할 때

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an API");
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1>{counter}</h1>

25175b868557db1670dc32a037d06bea069d1e4f
KDH <ekgns0131@daum.net>
2023-02-25 02:16:36 +0900

    BrowserRouter 과 HashRouter 차이점 BrowserRouter 사용 시-> localhost:3000/ HashRouter 사용 시-> localhost:3000/#/

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Screen from "./routes/Screen";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Screen />} />
       </Routes>
     </Router>
   );

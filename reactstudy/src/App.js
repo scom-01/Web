1b55277e5b571ad3ecd9b61bfa990d29c2481bf4
KDH <ekgns0131@daum.net>
2023-02-24 17:25:30 +0900

    event.preventDefault()로 form의 기본 함수(refresh)를 막고 setToDos함수로 input의 입력값을 받아 new [currentArray, todo]로 저장

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -1,18 +1,29 @@
 import { useState, useEffect } from "react";
-function Hello() {
-  useEffect(() => {
-    console.log("im here!");
-    return () => console.log("destroyed");
-  }, []);
-  return <h1>Hello</h1>;
-}
+
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [...currentArray, toDo]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello /> : null}
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type={"text"}
+          placeholder="Write your to do..."
+        />
+        <button>Add to Do</button>
+      </form>
     </div>
   );
 }

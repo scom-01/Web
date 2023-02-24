073634317c9d6aa6f581804c6cf888ad6dcb1d52
KDH <ekgns0131@daum.net>
2023-02-24 16:44:49 +0900

    useEffect DependencyList 두 번째 인자의 값이 변동 될 때만 첫 번째 인자인 함수 호출

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -2,14 +2,27 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, serKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => serKeyword(event.target.value);
   console.log("I run all the time");
   const iRunOnlyOnce = () => {
     console.log("I run only once");
   };
   useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type={"text"}
+        placeholder="Search hear..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click Me</button>
     </div>

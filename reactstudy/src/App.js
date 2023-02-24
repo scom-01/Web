a1237fd1a4f7d86af5216ec38523fa596291c6cb
KDH <ekgns0131@daum.net>
2023-02-24 16:55:41 +0900

    useEffect CleanUp func

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -1,30 +1,18 @@
 import { useState, useEffect } from "react";
-
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, serKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => serKeyword(event.target.value);
-  console.log("I run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("I run only once");
-  };
-  useEffect(iRunOnlyOnce, []);
+function Hello() {
   useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("Search for", keyword);
-    }
-  }, [keyword]);
+    console.log("im here!");
+    return () => console.log("destroyed");
+  }, []);
+  return <h1>Hello</h1>;
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type={"text"}
-        placeholder="Search hear..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click Me</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }

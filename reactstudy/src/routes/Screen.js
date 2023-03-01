2a2a6ba68429d2a5d40c261b89df30436ebdb348
KDH <ekgns0131@daum.net>
2023-03-01 19:02:05 +0900

    Add Movie.js to Screen.js

--- reactstudy/src/routes/Screen.js
+++ reactstudy/src/routes/Screen.js
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+
+import Movie from "../components/Movie";
+
 function Screen() {
   const [loading, setLoading] = useState(true);
-  const [movie, setmovie] = useState();
+  const [movie, setmovie] = useState([]);
   const { id } = useParams();
-  console.log(id);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    setmovie(json);
+    setmovie(json.data.movie);
     setLoading(false);
-    return;
   };
   useEffect(() => {
     getMovie();
@@ -22,9 +23,19 @@ function Screen() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <h1>
-          <Link to="/">Screen</Link>
-        </h1>
+        <div>
+          <h1>
+            <Link to="/">Screen</Link>
+          </h1>
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            coverImg={movie.medium_cover_image}
+            summary={movie.summary}
+            genres={movie.genres}
+          />
+        </div>
       )}
     </div>
   );

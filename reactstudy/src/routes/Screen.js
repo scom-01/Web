5c69493718ecf2b61ed4eab72b1f06931b8d8ab1
KDH <ekgns0131@daum.net>
2023-03-01 18:45:43 +0900

    url 에서 id값을 가져와서 movie data.json을 가져오면 `Loading...`이 사라지도록함. 또한 Screen.js에서  Link to=`/`를 이용하여 Home.js로 돌아가려할 때 useEffect(getMovie,[]);에서 useEffect(()=>{ getMovie() },[])로 변경하여 getMovie가 aysnc에서 `awit`기능으로 아직 반환값이 없을 때 return값이 필요하지만 중괄호인`{ }`로 return값을 매핑해서 오류를 해결

--- reactstudy/src/routes/Screen.js
+++ reactstudy/src/routes/Screen.js
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 function Screen() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setmovie] = useState();
   const { id } = useParams();
   console.log(id);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setmovie(json);
+    setLoading(false);
+    return;
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return (
-    <h1>
-      <Link to="/">Screen</Link>{" "}
-    </h1>
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <h1>
+          <Link to="/">Screen</Link>
+        </h1>
+      )}
+    </div>
   );
 }
 export default Screen;

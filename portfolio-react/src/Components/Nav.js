de29129098b61d1c62806d1f887d28be3dadd96d
KDH <ekgns0131@daum.net>
2023-03-02 21:57:50 +0900

    Add Topcheck Nav

--- portfolio-react/src/Components/Nav.js
+++ portfolio-react/src/Components/Nav.js
@@ -1,9 +1,28 @@
 import styles from "./Nav.module.css";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 function Nav() {
+  const [scrollTop, setScrollTop] = useState(true);
+  const updateScroll = () => {
+    setScrollTop(window.scrollY || document.documentElement.scrollTop);
+  };
+  useEffect(() => {
+    window.addEventListener(`scroll`, updateScroll);
+
+    return () => {
+      window.removeEventListener(`scroll`, updateScroll);
+    };
+  }, []);
+
   return (
-    <div className={styles.Nav}>
-      <Link to={"/"}>SCOM</Link>
+    <div>
+      {scrollTop ? (
+        "ss"
+      ) : (
+        <div className={styles.Nav}>
+          <Link to={"/"}>SCOM</Link>
+        </div>
+      )}
     </div>
   );
 }

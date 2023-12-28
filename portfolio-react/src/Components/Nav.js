dba6dd4c72f25fdd15b05d51b1411571979c80ea
KDH <ekgns0131@daum.net>
2023-12-29 06:28:41 +0900

    Fix Nav 네비게이션 및 css 코드작성

--- portfolio-react/src/Components/Nav.js
+++ portfolio-react/src/Components/Nav.js
@@ -4,46 +4,132 @@ import { useState, useEffect } from "react";
 function Nav() {
   const [scrollTop, setScrollTop] = useState(false);
   const [screenWidth, setScreenWidth] = useState(true);
+  const [LangKR, setLangKR] = useState(true);
+
   const updateScroll = () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY <= 50) {
       setScrollTop(true);
     } else setScrollTop(false);
   };
+  const updateScreenWidth = () => {
+    if (window.innerWidth >= 1420) setScreenWidth(true);
+    else setScreenWidth(false);
+  };
+
+  const updateLang = () => {
+    setLangKR((pre) => !pre);
+  };
   useEffect(() => {
     window.addEventListener(`scroll`, updateScroll);
-
+    window.addEventListener("resize", updateScreenWidth);
     return () => {
       window.removeEventListener(`scroll`, updateScroll);
+      window.removeEventListener("resize", updateScreenWidth);
     };
   }, []);
 
+  const menuItems = [
+    {
+      text: "about",
+      href: "/neowiz/about",
+      subLinks: [
+        { text: "we are", href: "/neowiz/about#we" },
+        { text: "연혁", href: "/neowiz/about#history" },
+      ],
+    },
+    {
+      text: "games",
+      href: "/neowiz/games",
+      subLinks: [
+        { text: "전체", href: "/neowiz/games/all" },
+        { text: "PC", href: "/neowiz/games/pc?list" },
+        { text: "모바일", href: "/neowiz/games/mobile?list" },
+        { text: "콘솔", href: "/neowiz/games/console?list" },
+        { text: "신규", href: "/neowiz/games/new?list" },
+      ],
+    },
+    {
+      text: "media",
+      href: "/neowiz/media",
+      subLinks: [
+        { text: "보도자료", href: "/neowiz/media/press?page=1&size=10" },
+        { text: "네오위즈 NOW", href: "/neowiz/media/story?page=1&size=9" },
+      ],
+    },
+    {
+      text: "investors",
+      href: "/neowiz/investors",
+      subLinks: [
+        { text: "주가", href: "/neowiz/investors#stock" },
+        { text: "재무", href: "/neowiz/investors#financial" },
+        { text: "실적", href: "/neowiz/investors#earningsrelease" },
+        { text: "공시", href: "/neowiz/investors#disclosure" },
+        { text: "공고", href: "/neowiz/investors#announce" },
+      ],
+    },
+    {
+      text: "carreers",
+      href: "/neowiz/careers",
+      subLinks: [
+        { text: "CAREERS", href: "/neowiz/careers" },
+        { text: "진행 중인 공고", href: "/neowiz/careers/recruit" },
+      ],
+    },
+    { text: "contact me", href: "/neowiz/contact-us" },
+  ];
+
   return (
     <div className={styles.Nav}>
-      <div className={styles.Nav__logo}>
-        <Link to={"/"}>Top</Link>
+      <div
+        className={`${scrollTop ? styles.Nav_top_true : styles.Nav_top_false}`}
+      >
+        <Link to={"/"} className={styles.Nav__logo} color="black">
+          <span>Top</span>
+        </Link>
+        {screenWidth && (
+          <div className={styles.Nav__menu}>
+            {menuItems.map((menuItem) => (
+              <li key={menuItem.text} className={styles.Nav_link}>
+                <Link to={menuItem.href}>
+                  <span>{menuItem.text}</span>
+                </Link>
+                {menuItem.subLinks && (
+                  <ul className={styles.Nav__submenu}>
+                    {menuItem.subLinks.map((subLink) => (
+                      <li key={subLink.text}>
+                        <Link to={subLink.href}>
+                          <span>{subLink.text}</span>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </div>
+        )}
+        <div className={styles.underline}></div>
+        {screenWidth ? (
+          <div className={styles.Nav_lang}>
+            <a
+              className={`${LangKR ? styles.on : ""}`}
+              onClick={!LangKR ? updateLang : undefined}
+            >
+              KR
+            </a>
+            <b style={{ color: "black" }}>|</b>
+            <a
+              className={`${!LangKR ? styles.on : ""}`}
+              onClick={LangKR ? updateLang : undefined}
+            >
+              EN
+            </a>
+          </div>
+        ) : (
+          <div className={styles.Nav__hambuger}>햄버거</div>
+        )}
+        <div className={styles.Nav__hiden}></div>
       </div>
-
-      <ul className={styles.Nav__menu}>
-        <li>
-          <Link to={"/"}>about</Link>
-        </li>
-        <li>
-          <Link to={"/"}>games</Link>
-        </li>
-        <li>
-          <Link to={"/"}>media</Link>
-        </li>
-        <li>
-          <Link to={"/"}>investors</Link>
-        </li>
-        <li>
-          <Link to={"/"}>carreers</Link>
-        </li>
-        <li>
-          <Link to={"/"}>contact me</Link>
-        </li>
-      </ul>
-      <a className={styles.Nav__hambuger}>햄버거</a>
     </div>
   );
 }

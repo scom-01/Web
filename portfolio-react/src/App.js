411a984fd592c6550842c58c73cdf66b353e8c0a
KDH <ekgns0131@daum.net>
2024-01-05 22:53:17 +0900

    Fix App.js

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,14 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import Home from "./Routes/Home";
 import TGB from "./Routes/TGB";
 import HIM from "./Routes/HIM";
+import About from "./Routes/About";
+
+const RedirectComponent = () => {
+  const navigate = useNavigate();
+  navigate("/");
+  React.useEffect(() => {
+    // 'https://새도메인.com' 부분을 새 도메인으로 변경해주세요
+    navigate("https://scom.com", { replace: true });
+  }, [navigate]);
+  return null;
+};
 
 function App() {
   console.log("render");
   return (
     <Router>
       <Routes>
+        <Route path="*" element={<RedirectComponent />} />
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
         <Route path="/Games/TGB" element={<TGB />} />
         <Route path="/Games/HIM" element={<HIM />} />
       </Routes>
--- portfolio-react/src/index.js
+++ portfolio-react/src/index.js
@@ -1,10 +1,14 @@
-import React from "react";
+import { React, StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
